0149c93e703eef1beda285d2ddcbf217dbd25ea2
unknown <carolinne.silva888@gmail.com>
2021-01-12 16:23:21 -0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Desafio
+{
+
+    public static void Main()
+    {
+
+        int a;
+        int b = 0;
+        int x;
+        int z;
+        int c = 0;
+        x = int.Parse(Console.ReadLine());
+        do
+        {
+            z = int.Parse(Console.ReadLine());
+        }
+
+        while (x > z + 1);
+        {
+
+            for (a = x; b < z; a++)
+            {
+
+                b += a;
+                c++;
+
+            }
+        }
+
+        Console.WriteLine(c);
+
+    }
+}
